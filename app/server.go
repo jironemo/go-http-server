4483900bd9aa86b236fd1bfb5cad029092614a4c
Aung Khaing Khant <aungkhaingkhant.dev@gmail.com>
2024-11-03 12:42:25 +0630

    Responding with 404

--- app/server.go
+++ app/server.go
@@ -20,25 +20,24 @@ func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
 	}
 
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
 	}
 	req := make([]byte, 1024)
 	_, err = conn.Read(req)
 	if err != nil {
 		fmt.Println("Failed to read data")
-		os.Exit(1)
 	}
 	requests := strings.Split(string(req), "\r\n")
 	uri := strings.Split(requests[0], " ")[1]
 	print(uri)
 	if uri != "/" {
-
-		conn.Write([]byte("HTTP/1.1 404 NotFound\r\n\r\n"))
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	}
+	conn.Close()
 }
